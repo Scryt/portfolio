712d6a9f89c430b5ed97fea48119615acdc3397d
Scryt <przemyslaw.tos@gmail.com>
2020-06-14 17:52:00 +0200

    Contact form including reCaptcha added. ReCaptcha uses general keys provided by google for tests purpose: https://developers.google.com/recaptcha/docs/faq#id-like-to-run-automated-tests-with-recaptcha.-what-should-i-do Further PHP develop required to handle data provided in form

--- js/my.js
+++ js/my.js
@@ -1,3 +1,15 @@
+var captchaCallback = function() {
+
+}
+
+var onloadCallback = function() {
+    grecaptcha.render('captcha', {
+        'sitekey' : '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+        'callback' : captchaCallback,
+        'theme': 'dark'
+    });
+};
+
 $(document).ready(function() {
     $("#about").load('content/text_files/about_page.txt');
 
@@ -9,5 +21,14 @@ $(document).ready(function() {
             scrollTop: target.offset().top - 120
         }, 1000);
         return false;
+    });
+
+    $("form").submit(function(event) {
+        if (grecaptcha.getResponse() === "") {
+            event.preventDefault();
+            $('#missing-captcha').html('Please check \"I\'m not a robot\" box: ');
+        } else {
+            $('#missing-captcha').html('');
+        }
     });
 });
